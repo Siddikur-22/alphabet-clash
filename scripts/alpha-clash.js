ab9a6af7cf12ca5fb9aafb6b38f8bb18c9725938
Siddikur22 <142910687+Siddikur22@users.noreply.github.com>
2024-02-14 02:38:46 +0600

    added button

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,4 +1,37 @@
 function play(){
-    console.log('play start now');
+    // hide the home screen..to hide the screen add the class hidden to the home screen
+
+    const homeSection = document.getElementById('home-screen');
+    homeSection.classList.add('hidden');
+    // console.log(homeSection.classList);
+
+    // show the playground
+    const playgroundSection = document.getElementById('play-ground');
+    // console.log(playgroundSection.classList);
+    playgroundSection.classList.remove('hidden');
+    continueGame()
+
+}
+
+function continueGame(){
+    // step 1: generate a random alphabet
+    const alphabet = getRandomAlphabet();
+    console.log('Your Random Alphabet :',alphabet);
+
+    // set random generated alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // set background color
+    setBackgroundColorById(alphabet);
+    
     
 }
+
+
+
+
+// function play(){
+//     hideElementById('home-screen');
+//     showElementById('play-ground');
+// }
---
+++ scripts/utility.js
@@ -0,0 +1,39 @@
+// function hideElementById(elementId){
+//     const element = document.getElementById(elementId);
+//     element.classlist.add('hidden');
+// }
+// function showElementById(elementId){
+//     const element = document.getElementById(elementId);
+//     element.classlist.remove('hidden');
+// }
+
+
+function getRandomAlphabet(){
+    // get or create an alphabet array
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    console.log(alphabets);
+
+    // get a random index generate between 0 -25 
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+    // console.log(index,alphabet);
+    return alphabet;
+}
+
+
+
+
+// add background color
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
